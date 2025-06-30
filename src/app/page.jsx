3d1051b7bf02bcef1a30bff2d83325fcf51db072
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-06-30 23:12:27 +0530

    Created BookList component and rendered cards on UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
+"use client";
+import BookList from "@/components/BookList";
 import Header from "@/components/Header";
 import Search from "@/components/Search";
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
   return (
     <div className="w-full min-h-screen bg-black text-white p-8">
       <Header />
-      <Search />
+      <Search setBooks={setBooks} />
+      <BookList books={books} />
     </div>
   );
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 import { Button } from "./ui/button";
 import getBooks from "@/lib/api";
 
-const Search = () => {
+const Search = ({ setBooks }) => {
   const [query, setQuery] = useState("");
   const handleSearch = async () => {
     if (!query.trim()) return;
     const res = await getBooks(query, 1);
+    setBooks(res);
     console.log(res);
+    
   };
   return (
     <div className="search w-full mt-5 flex items-center gap-6 justify-between">
