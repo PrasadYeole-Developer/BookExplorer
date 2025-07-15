afb93e3f67c3219297101840182b14110b8df77e
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-15 21:56:18 +0530

    Updated layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Books App | Discover and Explore Books",
   description:
-    "Books App lets you discover, search, and explore a wide range of books by can be also filtered by year. Find your next great read!",
+    "Books App lets you discover, search, and explore a wide range of books. You can also filter books by year to find your next great read!",
   keywords: [
     "books",
     "book app",
@@ -36,31 +36,20 @@ export const metadata = {
   openGraph: {
     title: "Books App | Discover and Explore Books",
     description:
-      "Books App lets you discover, search, and explore a wide range of books by year and author. Find your next great read!",
-    url: "https://your-books-app-url.com",
+      "Books App lets you discover, search, and explore a wide range of books. You can also filter books by year to find your next great read!",
+    url: "https://book-explorer-puce.vercel.app",
     siteName: "Books App",
-    images: [
-      {
-        url: "/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Books App Open Graph Image",
-      },
-    ],
     type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Books App | Discover and Explore Books",
-    description:
-      "Books App lets you discover, search, and explore a wide range of books by year and author. Find your next great read!",
-    images: ["/og-image.png"],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning className="transition-colors duration-200">
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className="transition-colors duration-200"
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
